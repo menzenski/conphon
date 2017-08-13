fc45a63c492ca676932579e2af0fd450749b762f
Matt Menzenski <matt@mobilgov.com>
2017-08-13 13:16:17 -0500

    use the new hotness pipfile and makefile

---
+++ setup.py
@@ -0,0 +1,40 @@
+"""
+A tool for applying sound change rules to linguistic data.
+"""
+import os
+import os.path
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+with open(os.path.join(here, "conphon", "__about__.py")) as about_file:
+    exec(about_file.read(), about)
+
+setup(
+    name='conphon',
+    version=about['__version__'],
+    url=about['__url__'],
+    license=about['__license__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    maintainer=about['__maintainer__'],
+    maintainer_email=about['__maintainer_email__'],
+    description=about['__description__'],
+    long_description=__doc__,
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    entry_points={
+        'console_scripts': [
+            'conphon = conphon.cli:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    setup_requires=['pytest-runner'],
+    tests_requires=['pytest'],
+)
